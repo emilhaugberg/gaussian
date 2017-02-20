5da8ed0e7d266e505498fa3e624d349758b96006
Emil Haugberg <emilveen@gmail.com>
2017-02-20 19:31:52 +0100

    Newtype instance for Matrix.

--- src/Data/Gaussian.purs
+++ src/Data/Gaussian.purs
@@ -6,13 +6,18 @@ import Control.Monad.Eff.Ref
 import Data.Array (length, nub, nubBy)
 import Data.Eq ((==))
 import Data.Foldable (foldl)
+import Data.Newtype (class Newtype, unwrap)
 import Data.Traversable (for)
-import Prelude (bind, pure, join, not, ($), (&&), (>))
+import Prelude (bind, pure, join, not, ($), (&&), (>), (<<<))
 
 newtype Matrix = Matrix (Array (Array Number))
+derive instance newtypeMatrix :: Newtype Matrix _
 
 isSufficient :: Matrix -> Boolean
-isSufficient (Matrix a) = 1 == length (nubBy (\xs ys -> length xs == length ys) a)
+isSufficient = (==) 1
+           <<< length
+           <<< nubBy (\xs ys -> length xs == length ys)
+           <<< unwrap
 
 gaussImpl :: forall e. Array (Array Number) -> Eff (ref :: REF | e) (Array Number)
 gaussImpl xs = do
@@ -21,6 +26,6 @@ gaussImpl xs = do
   pure $ join $ m'
 
 gauss :: forall e. Matrix -> Eff (ref :: REF, err :: EXCEPTION | e) (Array Number)
-gauss (Matrix m) = if not $ isSufficient (Matrix m)
-  then throw "Matrix is not sufficient"
-  else gaussImpl m
+gauss m = if   not $ isSufficient m
+          then throw "Matrix is not sufficient"
+          else gaussImpl $ unwrap m
