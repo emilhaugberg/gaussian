2296b47509fdf80a15756d23287afe68516980ba
Emil Haugberg <emilveen@gmail.com>
2017-02-21 01:24:26 +0100

    First time solved

--- src/Data/Gaussian.purs
+++ src/Data/Gaussian.purs
@@ -1,31 +1,95 @@
 module Data.Gaussian where
 
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Exception
-import Control.Monad.Eff.Ref
-import Data.Array (length, nub, nubBy)
-import Data.Eq ((==))
-import Data.Foldable (foldl)
+import Data.Array
+import Data.Either
+import Data.Int (toNumber)
+import Data.Maybe (Maybe(..), maybe, fromMaybe)
 import Data.Newtype (class Newtype, unwrap)
-import Data.Traversable (for)
-import Prelude (bind, pure, join, not, ($), (&&), (>), (<<<))
+import Data.Traversable
+import Data.Tuple (Tuple(..), fst, snd)
+import Prelude
 
-newtype Matrix = Matrix (Array (Array Number))
+type Row       = Array Number
+newtype Matrix = Matrix (Array Row)
+
 derive instance newtypeMatrix :: Newtype Matrix _
 
-isSufficient :: Matrix -> Boolean
-isSufficient = (==) 1
+type Length = Int
+
+indexFl :: forall a. Int -> Array a -> Maybe a
+indexFl = flip index
+
+subtractRow :: Row -> Row -> Row
+subtractRow = zipWith (-)
+
+eliminate :: Length -> Int -> Int -> Matrix -> Maybe Matrix
+eliminate length k i m = eliminate' m
+  where
+    eliminate' m = if k == length
+      then pure $ id m
+      else do
+        kElem <- join $ indexFl i <$> (indexFl k m')
+        iElem <- join $ indexFl i <$> (indexFl i m')
+        kRow  <- indexFl k m'
+        iRow  <- indexFl i m'
+
+        let r         = kElem / iElem
+        let newKRow   = subtractRow (map ((*) r) iRow) kRow
+        let newRows   = map (\r -> if r == kRow then newKRow else id r) m'
+
+        eliminate length (k + 1) i (Matrix (newRows))
+    m' = unwrap m
+
+pivot :: Length -> Int -> Int -> Matrix -> Maybe Matrix
+pivot length j i m = pivot' m
+  where
+    pivot' m = if j == length
+      then pure $ id m
+      else do
+        jElem <- join $ indexFl i <$> (indexFl j m')
+
+        if jElem == 0.0
+          then pivot length (j + 1) i m
+          else do
+            iRow <- indexFl i m'
+            jRow <- indexFl j m'
+
+            pure $ Matrix (flip iRow jRow m')
+    flip row1 row2 =
+      map (\r -> if r == row1 then row2 else if r == row2 then row1 else id r)
+    m' = unwrap m
+
+gauss' :: Int -> Matrix -> Maybe Matrix
+gauss' i m =
+  if i == n - 1
+    then pure $ id m
+    else do
+      iElem <- join $ indexFl i <$> (indexFl i m')
+
+      if iElem == 0.0
+        then do
+          mP   <- pivot     n (i + 1) i m
+          newM <- eliminate n (i + 1) i m
+          gauss' (i + 1) newM
+        else do
+          newM <- eliminate n (i + 1) i m
+          gauss' (i + 1) newM
+  where
+    m'  = unwrap m
+    nI  = length m' - 1
+    n   = length m'
+
+addToM :: Array Number -> Matrix -> Matrix
+addToM xs (Matrix m) = Matrix $ snoc m xs
+
+foreign import solver :: Int -> Matrix -> Array Number
+
+gauss :: Matrix -> Either String (Array Number)
+gauss m = if not $ isMatrix m
+  then Left "Matrix incorrect"
+  else maybe (Left "wrong") Right (solver (length $ unwrap m) <$> gauss' 0 m)
+  where
+    isMatrix = (==) 1
            <<< length
            <<< nubBy (\xs ys -> length xs == length ys)
            <<< unwrap
-
-gaussImpl :: forall e. Array (Array Number) -> Eff (ref :: REF | e) (Array Number)
-gaussImpl xs = do
-  m  <- newRef xs
-  m' <- readRef m
-  pure $ join $ m'
-
-gauss :: forall e. Matrix -> Eff (ref :: REF, err :: EXCEPTION | e) (Array Number)
-gauss m = if   not $ isSufficient m
-          then throw "Matrix is not sufficient"
-          else gaussImpl $ unwrap m
--- src/Main.purs
+++ src/Main.purs
@@ -1,5 +1,8 @@
 module Main where
 
+import Data.Array
+import Data.Newtype
+import Data.Either
 import Data.Gaussian (Matrix(..), gauss)
 import Prelude
 import Control.Monad.Eff (Eff)
@@ -7,13 +10,15 @@ import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Ref
 import Control.Monad.Eff.Exception
 
-main :: forall e. Eff (console :: CONSOLE, err :: EXCEPTION, ref :: REF | e) Unit
+showMatrix :: Matrix -> String
+showMatrix (Matrix xs) = "Matrix: " <> show xs
+
+main :: forall e. (Partial) => Eff (console :: CONSOLE, err :: EXCEPTION, ref :: REF | e) Unit
 main = do
-  x <- gauss m
-  log $ show $ x
+  log $ show $ fromRight $ gauss m
+  log $ show $ length $ unwrap m
   where
     m = Matrix
-      [ [ 1.0, 2.0, 3.0 ]
-      , [ 2.0, 3.0, 4.0 ]
-      , [ 2.0, 5.0, 6.0 ]
+      [ [ 2.0, 3.0,  13.0 ]
+      , [ 1.0, -1.0, -1.0 ]
       ]
